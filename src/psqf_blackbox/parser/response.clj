f40e98ec8fbb822f7b6683b650c3d9fb2fcb1890
Ed Bowler <edward.bowler@skybettingandgaming.com>
2017-03-22 14:43:30 +0000

    set length

--- src/psqf_blackbox/parser/response.clj
+++ src/psqf_blackbox/parser/response.clj
@@ -9,5 +9,17 @@
                         ret-addr/structure)
                 (filter keyword?)))
 
+(def header-structure (->> structure
+                           (filter #(-> % namespace (.startsWith "psqf-blackbox.parser.header")))))
+
+(def body-structure (->> structure
+                         (filter #(-> % namespace (.startsWith "psqf-blackbox.parser.body")))))
+
+(def attrs-structure (->> structure
+                          (filter #(-> % namespace (.startsWith "psqf-blackbox.parser.attrs")))))
+
 (defn serialise [r]
-  [r (byte-array (mapcat ::c/raw (map r structure)))])
+  (let [body   (mapcat ::c/raw (map r body-structure))
+        attrs  (mapcat ::c/raw (map r attrs-structure))
+        header (mapcat ::c/raw (map (assoc r ::h/body-length (count body) ::h/attribute-length (count attrs)) header-structure))]
+    [r (byte-array (concat header body attrs))]))
