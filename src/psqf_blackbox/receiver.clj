6d4bfa0caa0026c21098f091d402b6f6e56d41ca
Peter Phillips <peter@greenthistle.co.uk>
2017-03-22 07:08:26 +0000

    Add in async channels for messages

--- src/psqf_blackbox/receiver.clj
+++ src/psqf_blackbox/receiver.clj
@@ -1,23 +1,43 @@
 (ns psqf-blackbox.receiver
   (:import [java.net DatagramSocket DatagramPacket InetAddress])
-  (:require [psqf-blackbox.parser :as p]))
+  (:require [psqf-blackbox.parser :as bp]
+            [psqf-blackbox.controller :as bc]
+            [clojure.core.async :as a]
+            [psqf-blackbox.parser :as p]))
 
 (def socket_in (DatagramSocket. 5200))
 
 (def running (atom true))
 (def buffer (make-array Byte/TYPE 1024))
-(defn parse [packet]
-  {:ip    (InetAddress/getByName "127.0.0.1")
-   :port  5201
-   :bytes (.getBytes (String. (.getData packet) 0 (.getLength packet)))})
+
+(defn control []
+  (let [in (a/chan 10)
+        out (a/chan 10)]
+    (a/go (while true
+            (let [input (a/<! in)
+                  output (p/parser input)]
+              (prn output)
+              (a/>! out output)
+              )))
+    [in out]))
 
 (defn start-receiver []
   (while (true? @running)
-    (let [packet (DatagramPacket. buffer 1024)]
-      (println "running here")
+    (let [packet (DatagramPacket. buffer 1024)
+          [in out] (control)]
+      (prn "starting receiver")
       (.receive socket_in packet)
-      (let [{:keys [ip port bytes]} (parse packet)]
-        (.send (DatagramSocket.) (DatagramPacket. bytes (count bytes) ip port))
-        )
-      )
-    ))
+      (a/>!! in (.getData packet))
+      (a/go
+        (let [{:keys [ip port bytes]} (a/<! out)]
+          (.send (DatagramSocket.) (DatagramPacket. bytes (count bytes) (InetAddress/getByName "127.0.0.1") 5201))
+          )
+        ;)
+        ))))
+
+(defn stop-receiver []
+  (prn "stopping receiver")
+  (reset! @running false)
+  (.close socket_in))
+
+
