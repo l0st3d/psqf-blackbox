13166d37a34edce4a17748590b1451486b7ccf94
Ed Brindley <edward.brindley@skybettingandgaming.com>
2017-03-22 12:50:09 +0000

    More worker stuff

--- src/psqf_blackbox/worker.clj
+++ src/psqf_blackbox/worker.clj
@@ -1,9 +1,17 @@
 (ns psqf-blackbox.worker
   (:require [psqf-blackbox.parser :as bp]
+            [psqf-blackbox.parser.common :as c]
             [clojure.core.async :as a]
-            [psqf-blackbox.parser :as p]))
+            [psqf-blackbox.parser :as p]
+            [psqf-blackbox.parser.header :as h]
+             [psqf-blackbox.parser.body :as b]))
 
-(defn do-work [input-defs]
-  (prn input-defs)
-  (Thread/sleep 5000)
-  (.getBytes "hello"))
+(defmulti do-work (juxt (comp ::c/int ::h/service-id) (comp ::c/int ::h/message-id)))
+(defmethod do-work
+  [7900 1]
+  [parsed-message]
+  ;(prn parsed-message)
+
+  (let [header (select-keys parsed-message (filter keyword? h/structure))]
+    (merge header {::b/receipt 1234 ::b/responsecode 1024})))
+(defmethod do-work [7900 2] [parsed-message] {})
