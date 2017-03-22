e05a5738ec74a62615f40838d489ceabb0649157
Ed Bowler <edward.bowler@skybettingandgaming.com>
2017-03-22 14:18:37 +0000

    added dep

--- src/user.clj
+++ src/user.clj
@@ -5,4 +5,5 @@
             [clojure.spec :as s]
             [clojure.string :as st]
             [clojure.java.io :as io]
-            [clojure.core.async :as a]))
+            [clojure.core.async :as a]
+            [clojure.data :as data]))
