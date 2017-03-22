cf01e439dfb275884524f76d5ca0bae4ba871645
Ed Bowler <edward.bowler@skybettingandgaming.com>
2017-03-22 15:09:18 +0000

    deal with non-parsed inputs

--- src/psqf_blackbox/parser/response.clj
+++ src/psqf_blackbox/parser/response.clj
@@ -20,8 +20,21 @@
 (def attrs-structure (->> structure
                           (filter #(-> % namespace (.startsWith "psqf-blackbox.parser.attrs")))))
 
+(defn int->bytes [i]
+  (loop [acc ()
+         i i]
+    (if (-> i (> 0))
+      (recur (cons (mod i 256) acc) (int (/ i 256)))
+      acc)))
+
+(defn- get-raw [v]
+  (cond (::c/raw v)  (::c/raw v)
+        (integer? v) (int->bytes v)
+        (string? v)  (.getBytes v)
+        :else        (throw (ex-info "Cannot get raw value" {:data v}))))
+
 (defn serialise [r]
-  (let [body   (mapcat ::c/raw (map r body-structure))
-        attrs  (mapcat ::c/raw (map r attrs-structure))
-        header (mapcat ::c/raw (map (assoc r ::h/body-length (count body) ::h/attribute-length (count attrs)) header-structure))]
+  (let [body   (mapcat get-raw (map r body-structure))
+        attrs  (mapcat get-raw (map r attrs-structure))
+        header (mapcat get-raw (map (assoc r ::h/body-length (count body) ::h/attribute-length (count attrs)) header-structure))]
     [r (byte-array (concat header body attrs))]))
